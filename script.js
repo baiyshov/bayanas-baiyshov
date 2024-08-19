20e6b4e19e0cd730b2671c69d320b7f92fec02a8
baiyshov <bayanasbaiyshov2023@gmail.com>
2024-08-20 00:26:27 +0600

    Байышов Баянас Илиясбекович

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
